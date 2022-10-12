4889aa357788de16f547d6bd1e4fd3f37580edcd
nwhite <c2lkn.t@gmail.com>
2022-10-12 10:40:08 -0500

    updating gitignore further

--- wgetting/splitter-uploader.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-mkdir splitfiles
-split -l 1000 metadata.csv splitfiles/metadata- --additional-suffix=.csv
-for f in splitfiles/*.csv
-do
-    sed -i '1iidentifier,notes' $f
-    # ./ia ....
-done
