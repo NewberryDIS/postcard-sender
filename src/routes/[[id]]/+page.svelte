fe460b108897dfa3d69bd5d6b0b15b335d075e93
Nick White <nick33white@gmail.com>
2024-02-02 14:07:33 -0600

    fix missing header css

--- src/routes/[[id]]/+page.svelte
+++ src/routes/[[id]]/+page.svelte
@@ -1,15 +1,15 @@
 <script lang="ts">
 
-    export let data
-    // console.log(data.postcards)
-    import {base} from '$app/paths'
-    import { imgUrl } from '$lib'
-    import NewberryDCLogo from "$lib/NewberryDCLogo.svelte" 
-    import Gallery from "$lib/Gallery.svelte" 
-    import ShareButtons from "$lib/ShareButtons.svelte" 
+export let data
+// console.log(data.postcards)
+import {base} from '$app/paths'
+import { imgUrl } from '$lib'
+import NewberryDCLogo from "$lib/NewberryDCLogo.svelte" 
+import Gallery from "$lib/Gallery.svelte" 
+import ShareButtons from "$lib/ShareButtons.svelte" 
 </script>
 
-    <svelte:head><title>Newberry Postcards</title></svelte:head>
+<svelte:head><title>Newberry Postcards</title></svelte:head>
 
 <main>
     <div class="logo-wrapper">
@@ -25,7 +25,7 @@
         <section>
             <ShareButtons />
         </section>
-            <dark-mode-toggle></dark-mode-toggle>
+        <dark-mode-toggle></dark-mode-toggle>
         <!-- <img src="{base}/webp/{data.itemData.image}.webp" alt="" /> -->
     </div>
     <div class="gallery">
@@ -34,68 +34,67 @@
 </main>
 
 <style>
-    .logo-wrapper {
-        position: absolute;
-        top: 11px;
-        left: 11px;
-        z-index: 9001;
-    }
-    h1, p {
-        margin: 0;
-        text-align: center;
-    }
-    h1 {
-        max-width: calc(100vw - 100px);
-font-size: clamp(2rem, 0.4321rem + 5.5749vw, 4rem);
-    }
-    p {
-font-size: clamp(1rem, 0.7413rem + 0.9199vw, 1.33rem);
-    }
-    main {
-        display: flex;
-        gap: 11px;
+.logo-wrapper {
+    position: absolute;
+    top: 11px;
+    left: 11px;
+    z-index: 9001;
+}
+h1, p {
+    margin: 0;
+    text-align: center;
+}
+h1 {
+    max-width: calc(100vw - 100px);
+    font-size: clamp(2rem, 0.4321rem + 5.5749vw, 4rem);
+}
+p {
+    font-size: clamp(1rem, 0.7413rem + 0.9199vw, 1.33rem);
+}
+main {
+    display: flex;
+    gap: 11px;
 
-    }
-    .featured-image{
-        object-fit: contain;
-        /* max-height: calc(90% - 60px); */
-        flex: auto;
-    }
-    .caption {
-        margin: 11px;
-    }
-    .content {
-        position: relative;
-        flex: 1;
-        min-width: 0;
-        min-height: 0;
-        box-sizing: border-box;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: end;
-        gap: 11px;
+}
+.featured-image{
+    object-fit: contain;
+    /* max-height: calc(90% - 60px); */
+    flex: auto;
+}
+.caption {
+    margin: 11px;
+}
+.content {
+    position: relative;
+    flex: 1;
+    min-width: 0;
+    min-height: 0;
+    box-sizing: border-box;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: end;
+    gap: 11px;
 
-    }
-    section, header {
-        min-width: 0;
-        min-height: 0;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-    }
-    section :is(img, p) {
-        min-width: 0;
-        min-height: 0;
-    } 
-    section {
-        text-align: center;
-        /* max-width: 90%; */
-    }
-    section p {
-        padding: 0;
-        margin: 0;
-    }
-header
+}
+section, header {
+    min-width: 0;
+    min-height: 0;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+}
+section :is(img, p) {
+    min-width: 0;
+    min-height: 0;
+} 
+section {
+    text-align: center;
+    /* max-width: 90%; */
+}
+section p {
+    padding: 0;
+    margin: 0;
+}
 </style>
